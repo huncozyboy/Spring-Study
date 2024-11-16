0fd23445303a60d4147b3557247cfd30ad88227d
huncozyboy <leejh5838@naver.com>
2024-11-17 02:19:53 +0900

    fix : 오타수정

---
+++ src/main/java/hello/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package hello.repository;
+
+import hello.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
