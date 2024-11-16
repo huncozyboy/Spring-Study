e18050409437c8fb15f77ed71b00186c4d7627ec
huncozyboy <leejh5838@naver.com>
2024-11-17 02:20:00 +0900

    fix : 오타수정

--- src/main/java/hello/repository/BlogRepsotiry.java
+++
@@ -1,7 +0,0 @@
-package hello.repository;
-
-import hello.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepsotiry extends JpaRepository<Article, Long> {
-}
