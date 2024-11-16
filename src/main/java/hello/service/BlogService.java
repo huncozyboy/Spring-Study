5eb28a171e8f1b3d3638fe4a4f9ff7f5f84a6f1e
huncozyboy <leejh5838@naver.com>
2024-11-17 02:20:10 +0900

    feat : 블로그 서비스 구현

---
+++ src/main/java/hello/service/BlogService.java
@@ -0,0 +1,17 @@
+package hello.service;
+
+import hello.repository.BlogRepository;
+import hello.domain.Article;
+import hello.dto.AddArticleRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
