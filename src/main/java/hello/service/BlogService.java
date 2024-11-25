f8299733dc968009688032ff8f355c07cc949843
huncozyboy <leejh5838@naver.com>
2024-11-25 22:59:51 +0900

    refactor : 불필요한 메소드 삭제

--- src/main/java/hello/service/BlogService.java
+++ src/main/java/hello/service/BlogService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogService {
     private final BlogRepository blogRepository;
-    //블로그 서비스
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
     }
