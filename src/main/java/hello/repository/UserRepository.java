8e50510ac9b471b9b16bb30d22ea8598e6bda8d0
huncozyboy <leejh5838@naver.com>
2024-12-14 20:35:00 +0900

    feat : 유저 레포 생성

---
+++ src/main/java/hello/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package hello.repository;
+
+import java.util.Optional;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+        Optional<User> findIdById(Long userId);
+}
