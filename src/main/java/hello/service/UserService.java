e65bfdbbd5b60be27db1a52f8e566d29ee289530
huncozyboy <leejh5838@naver.com>
2024-12-14 20:35:31 +0900

    feat : 유저 서비스 구현

---
+++ src/main/java/hello/service/UserService.java
@@ -0,0 +1,10 @@
+package hello.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+}
