d951ded440cb47678e7d4445d2f16d762745a559
huncozyboy <leejh5838@naver.com>
2024-12-14 20:36:05 +0900

    feat : 유저 엔티티 추가

---
+++ src/main/java/hello/domain/User.java
@@ -0,0 +1,25 @@
+package hello.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
