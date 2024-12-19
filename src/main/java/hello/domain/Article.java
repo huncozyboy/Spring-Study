560755f51e601716a132369b73df33e7f3b7905a
huncozyboy <leejh5838@naver.com>
2024-12-19 13:15:27 +0900

    refactor : 생성, 수정시간 추가

--- src/main/java/hello/domain/Article.java
+++ src/main/java/hello/domain/Article.java
@@ -2,13 +2,19 @@ package hello.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor
@@ -23,8 +29,21 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @Builder
     public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
