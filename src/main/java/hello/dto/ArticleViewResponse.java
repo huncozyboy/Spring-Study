545a898cc386cc536bb8d00c712418262b50d545
huncozyboy <leejh5838@naver.com>
2024-12-19 13:16:14 +0900

    feat : 블로그 뷰 응답 dto 추가

---
+++ src/main/java/hello/dto/ArticleViewResponse.java
@@ -0,0 +1,22 @@
+package hello.dto;
+
+import hello.domain.Article;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
