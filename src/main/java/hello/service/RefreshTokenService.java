332ddbd7d1a9641eff992f6a5a23f9880d7ed937
huncozyboy <leejh5838@naver.com>
2024-12-26 11:58:48 +0900

    feat : 토큰 관련 서비스 구현

---
+++ src/main/java/hello/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package hello.service;
+
+import hello.domain.RefreshToken;
+import hello.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
---
+++ src/main/java/hello/service/TokenService.java
@@ -0,0 +1,27 @@
+package hello.service;
+
+import hello.config.jwt.TokenProvider;
+import hello.domain.User;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) {
+        // 토큰 유효성 검사에 실패하면 예외 발생
+        if(!tokenProvider.validToken(refreshToken)) {
+            throw new IllegalArgumentException("Unexpected token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
+}
