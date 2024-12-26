c0e20a4287b22794d946ba5786a5116c0ad6ee78
huncozyboy <leejh5838@naver.com>
2024-12-26 11:58:28 +0900

    feat : 리프레시 토큰 레포 구현

---
+++ src/main/java/hello/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package hello.repository;
+
+import hello.domain.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
