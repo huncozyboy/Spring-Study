c7d44a4c20679d3ac2cd5951b4fa92f398cce51c
huncozyboy <leejh5838@naver.com>
2024-12-26 11:59:52 +0900

    feat : 토큰 컨트롤러 구현

---
+++ src/main/java/hello/controller/TokenApiController.java
@@ -0,0 +1,27 @@
+package hello.controller;
+
+import hello.dto.CreateAccessTokenRequest;
+import hello.dto.CreateAccessTokenResponse;
+import hello.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
